908ca448542f70b73c4c878fb8eeecf52c3fd00b
italiya07 <drashtiitaliya25@gmail.com>
2023-11-15 17:04:46 -0500

    Created Index page, Navigation bar, Crypto Listing on index page

--- CoinEx_project/mainApp/views.py
+++ CoinEx_project/mainApp/views.py
@@ -1,3 +1,67 @@
+# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+
+    cryptos = [
+        {
+            "name": "Bitcoin",
+            "Price": 34500,
+            "oneHour": "0.27%",
+            "oneHourFlag": -1,
+            "TwentyFourHour": "0.67",
+            "TwentyFourHourFlag": -1,
+            "MarketCap": "673,506,667,886",
+            "Volume": "17,354,758,340"
+        },
+        {
+            "name": "Ethereum",
+            "Price": 2600,
+            "oneHour": "0.42%",
+            "oneHourFlag": 1,
+            "TwentyFourHour": "1.25%",
+            "TwentyFourHourFlag": 1,
+            "MarketCap": "300,456,789,123",
+            "Volume": "10,456,123,789"
+        },
+        {
+            "name": "Solana",
+            "Price": 1.23,
+            "oneHour": "0.11%",
+            "oneHourFlag": -1,
+            "TwentyFourHour": "0.75%",
+            "TwentyFourHourFlag": -1,
+            "MarketCap": "56,789,123,456",
+            "Volume": "2,345,678,901"
+        },
+        {
+            "name": "XRP",
+            "Price": 155,
+            "oneHour": "0.33%",
+            "oneHourFlag": 1,
+            "TwentyFourHour": "0.85%",
+            "TwentyFourHourFlag": 1,
+            "MarketCap": "12,345,678,901",
+            "Volume": "567,890,123"
+        },
+        {
+            "name": "BNB",
+            "Price": 2.45,
+            "oneHour": "0.28%",
+            "oneHourFlag": 1,
+            "TwentyFourHour": "0.92%",
+            "TwentyFourHourFlag": 1,
+            "MarketCap": "7,890,123,456",
+            "Volume": "345,678,901"
+        }
+    ]
+
+    context = {
+        "cryptos": cryptos
+    }
+    return render(request, 'CoinEx_Index/index.html', context=context)
+
+
