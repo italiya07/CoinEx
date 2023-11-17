864562aaa84ab4ec74d728fe6a38541eed4555c2
italiya07 <drashtiitaliya25@gmail.com>
2023-11-17 13:59:51 -0500

    dashboard url change

--- CoinEx_project/mainApp/urls.py
+++ CoinEx_project/mainApp/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from .views import dashboard, crypto_list, fear_and_greed_index, news_list
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -9,4 +8,3 @@ urlpatterns = [
     path('fear_and_greed/', fear_and_greed_index, name='fear_and_greed_index'),
     path('news/', news_list, name='news_list'),
 ]
-
