cc6132ee3b7366bae47a5a1f93208f0dbbd40dea
Navjot Makkar <makkar.navjot@gmail.com>
2023-11-16 19:58:42 -0500

    Added the model for FearAndGreedIndex and News and applied migrations and updated urls file

--- CoinEx_project/mainApp/models.py
+++ CoinEx_project/mainApp/models.py
@@ -4,3 +4,17 @@ class Crypto(models.Model):
     name = models.CharField(max_length=100)
     symbol = models.CharField(max_length=10)
     today_price = models.DecimalField(max_digits=10, decimal_places=2)
+    def __str__(self):
+        return self.name
+
+class FearAndGreedIndex(models.Model):
+    date = models.DateField()
+    value = models.IntegerField()
+    def __str__(self):
+        return f'{self.date} - {self.value}'
+
+class News(models.Model):
+    title = models.CharField(max_length=500)
+    link = models.URLField()
+    def __str__(self):
+        return self.title
