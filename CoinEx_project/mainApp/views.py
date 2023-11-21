490d60dbb8dfd8888b482b69ed21a2e88323bfd4
Navjot Makkar <makkar.navjot@gmail.com>
2023-11-20 21:13:36 -0500

    Updating the top crypto, latest news, and fear and greed index django views, html tags and design

--- CoinEx_project/mainApp/views.py
+++ CoinEx_project/mainApp/views.py
@@ -9,20 +9,36 @@ from django.contrib.auth import login as django_login, authenticate
 
 def index(request):
     cryptos = Cryptocurrency.objects.all()
-    # Retrieve top 5 cryptocurrencies for the highlight box
-    top_cryptos = Cryptocurrency.objects.all()[:5]
 
-    # Retrieve latest 5 news for the highlight box
-    latest_news = News.objects.all()[:5]
+    # Calculate topness scores for all cryptocurrencies
+    cryptos_with_topness = [
+        (crypto, calculate_topness(crypto))
+        for crypto in cryptos
+    ]
+
+    # Sort cryptocurrencies based on topness scores in descending order
+    sorted_cryptos = sorted(cryptos_with_topness, key=lambda x: x[1], reverse=True)
+
+    # Retrieve top 5 cryptocurrencies
+    top_cryptos = [crypto for crypto, _ in sorted_cryptos[:5]]
+
+    # Retrieve latest 5 news based on published date
+    latest_news = News.objects.order_by('-published_date')[:5]
 
     # Retrieve the latest Fear & Greed Index value
     latest_index = FearAndGreedIndex.objects.latest('date')
+
+    # Set a static value for testing
+    #static_index_value = 35  # You can change this to any value for testing
+
     context = {
         "cryptos": cryptos,
         "top_cryptos": top_cryptos,
         "latest_news": latest_news,
+        #"latest_index": FearAndGreedIndex(value=static_index_value),
         "latest_index": latest_index
     }
+
     return render(request, 'CoinEx_Index/index.html', context=context)
 
 def register(request):
@@ -62,3 +78,21 @@ def fear_and_greed_index(request):
 def news_list(request):
     all_news = News.objects.all()
     return render(request, 'CoinEx_Index/news_list.html', {'all_news': all_news})
+
+def calculate_topness(crypto):
+    # Define weights for each factor
+    weight_change = 0.4
+    weight_volume = 0.3
+    weight_market_cap = 0.3
+
+    # Convert Decimal values to float for calculation
+    change = float(crypto.twenty_four_hour_change)
+    volume = float(crypto.volume)
+    market_cap = float(crypto.market_cap)
+
+    # Calculate the topness score using the weights
+    topness_score = (change * weight_change +
+                     volume * weight_volume +
+                     market_cap * weight_market_cap)
+
+    return topness_score
